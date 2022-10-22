17999158de7587930f0e596ab16a4ea30585f325
Julieta Mangalavite <julimangalavite@gmail.com>
2022-10-22 14:25:57 -0300

    Agregando JS_01

---
+++ js/integrador.js
@@ -0,0 +1,48 @@
+
+const btnResumen = document.getElementById("btn-resumen");
+const btnResetear = document.getElementById("btnResetear");
+
+
+const cant = document.getElementById("cantidad");
+
+function total() {
+    const categoria = document.getElementById("categoria");
+    let totalPagar = 0;
+
+    if (cant.value >= 1) {
+        
+        switch (categoria.value) {
+
+            case "Estudiante":
+            totalPagar= (40 * cant.value);
+            break
+            
+            case "Trainee":
+                
+                totalPagar= (100 * cant.value);
+            break
+            
+            case "Junior":
+               
+                totalPagar= (170 * cant.value);
+            break
+            
+            }
+    const elTotal = document.getElementById("resultado");
+    return elTotal.innerHTML = "Total a pagar: $" + totalPagar;
+
+    } else {
+    const elTotal = document.getElementById("resultado");
+    return elTotal.innerHTML = "Ingrese una cantidad";
+    }   
+
+}
+
+
+/*function reset() {
+    const resetCant = document.getElementById("cantidad");
+    return resetCant.innerHTML = "0"; 
+}
+
+btnResetear.onclick = reset; */
+btnResumen.onclick = total;
